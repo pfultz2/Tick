8a34423a0af17d5f5678f6676b76e031c02a38f9
Paul <pfultz2@yahoo.com>
2014-11-14 19:58:43 -0500

    Update integral constant to have all of the operators overloaded

---
+++ test/integral_constant.cpp
@@ -0,0 +1,73 @@
+
+
+#include "test.h"
+#include <tick/integral_constant.h>
+
+TICK_STATIC_TEST_CASE()
+{
+    typedef tick::integral_constant<bool, true> true_type;
+    static_assert(true_type(), "Failed");
+    typedef tick::integral_constant<bool, false> false_type;
+    static_assert(!false_type(), "Failed");
+
+    typedef tick::integral_constant<int, 1> one_type;
+    typedef tick::integral_constant<int, 2> two_type;
+
+    typedef decltype(!true_type()) not_true_type;
+    static_assert(not_true_type::value == false, "Failed");
+
+    typedef decltype(true_type() && false_type()) true_type_and_false_type;
+    static_assert(true_type_and_false_type::value == false, "Failed");
+    typedef decltype(true_type() || false_type()) true_type_or_false_type;
+    static_assert(true_type_or_false_type::value == true, "Failed");
+
+    typedef decltype(!two_type()) not_two_type;
+    static_assert(not_two_type::value == (!2), "Failed");
+
+// GCC confuses the complement operator with the destructor
+#ifdef __clang__
+    typedef decltype(~two_type()) compl_two_type;
+    static_assert(compl_two_type::value == (~2), "Failed");
+#endif
+
+    typedef decltype(+two_type()) unary_plus_two_type;
+    static_assert(unary_plus_two_type::value == (+2), "Failed");
+    typedef decltype(-two_type()) unary_subtract_two_type;
+    static_assert(unary_subtract_two_type::value == (-2), "Failed");
+
+    typedef decltype(one_type() + two_type()) one_type_add_two_type;
+    static_assert(one_type_add_two_type::value == (1 + 2), "Failed");
+    typedef decltype(one_type() - two_type()) one_type_subtract_two_type;
+    static_assert(one_type_subtract_two_type::value == (1 - 2), "Failed");
+    typedef decltype(one_type() * two_type()) one_type_multiply_two_type;
+    static_assert(one_type_multiply_two_type::value == (1 * 2), "Failed");
+    typedef decltype(one_type() / two_type()) one_type_divide_two_type;
+    static_assert(one_type_divide_two_type::value == (1 / 2), "Failed");
+    typedef decltype(one_type() % two_type()) one_type_remainder_two_type;
+    static_assert(one_type_remainder_two_type::value == (1 % 2), "Failed");
+    typedef decltype(one_type() >> two_type()) one_type_shift_right_two_type;
+    static_assert(one_type_shift_right_two_type::value == (1 >> 2), "Failed");
+    typedef decltype(one_type() << two_type()) one_type_shift_left_two_type;
+    static_assert(one_type_shift_left_two_type::value == (1 << 2), "Failed");
+    typedef decltype(one_type() > two_type()) one_type_greater_than_two_type;
+    static_assert(one_type_greater_than_two_type::value == (1 > 2), "Failed");
+    typedef decltype(one_type() < two_type()) one_type_less_than_two_type;
+    static_assert(one_type_less_than_two_type::value == (1 < 2), "Failed");
+    typedef decltype(one_type() <= two_type()) one_type_less_than_equal_two_type;
+    static_assert(one_type_less_than_equal_two_type::value == (1 <= 2), "Failed");
+    typedef decltype(one_type() >= two_type()) one_type_greater_than_equal_two_type;
+    static_assert(one_type_greater_than_equal_two_type::value == (1 >= 2), "Failed");
+    typedef decltype(one_type() == two_type()) one_type_equal_two_type;
+    static_assert(one_type_equal_two_type::value == (1 == 2), "Failed");
+    typedef decltype(one_type() != two_type()) one_type_not_equal_two_type;
+    static_assert(one_type_not_equal_two_type::value == (1 != 2), "Failed");
+    typedef decltype(one_type() & two_type()) one_type_bit_and_two_type;
+    static_assert(one_type_bit_and_two_type::value == (1 & 2), "Failed");
+    typedef decltype(one_type() ^ two_type()) one_type_xor_two_type;
+    static_assert(one_type_xor_two_type::value == (1 ^ 2), "Failed");
+    typedef decltype(one_type() | two_type()) one_type_bit_or_two_type;
+    static_assert(one_type_bit_or_two_type::value == (1 | 2), "Failed");
+    
+    
+
+};
--- tick/integral_constant.h
+++ tick/integral_constant.h
@@ -10,14 +10,64 @@
 
 #include <type_traits>
 
+
 namespace tick {
 
 template<class T, T v>
 struct integral_constant
 : std::integral_constant<T, v>
 {
+    constexpr integral_constant()
+    {}
+
+    template<class X, typename std::enable_if<(
+        std::is_same<T, typename X::value_type>::value and X::value == v
+    )>::type>
+    constexpr integral_constant(X)
+    {}
+
     constexpr T operator()() const noexcept { return v; }
 };
+
+#define TICK_INTEGRAL_CONSTANT_BINARY_OP(op) \
+template<class T, T v, class U, U w> \
+constexpr inline integral_constant<decltype(v op w), (v op w)> \
+operator op(integral_constant<T, v>, integral_constant<U, w>) noexcept \
+{ \
+    return {}; \
+}
+
+#define TICK_INTEGRAL_CONSTANT_UNARY_OP(op) \
+template<class T, T v> \
+constexpr inline integral_constant<decltype(op v), (op v)> \
+operator op(integral_constant<T, v>) noexcept \
+{ \
+    return {}; \
+}
+
+TICK_INTEGRAL_CONSTANT_BINARY_OP(+)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(-)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(*)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(/)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(%)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(>>)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(<<)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(>)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(<)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(<=)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(>=)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(==)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(!=)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(&)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(^)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(|)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(&&)
+TICK_INTEGRAL_CONSTANT_BINARY_OP(||)
+
+TICK_INTEGRAL_CONSTANT_UNARY_OP(!)
+TICK_INTEGRAL_CONSTANT_UNARY_OP(~)
+TICK_INTEGRAL_CONSTANT_UNARY_OP(+)
+TICK_INTEGRAL_CONSTANT_UNARY_OP(-)
 
 }
 
