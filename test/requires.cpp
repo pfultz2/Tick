cbb9a6e1180f75548472d141852e283d4f3c20cc
Paul <pfultz2@yahoo.com>
2016-01-30 17:42:06 -0800

    Add more tests and workarounds for requires

--- test/requires.cpp
+++ test/requires.cpp
@@ -6,6 +6,13 @@
 
 struct not_int {};
 
+template<class T>
+struct tick_is_integral
+: tick::integral_constant<bool, (std::is_integral<T>::value)>
+{
+    constexpr tick_is_integral()
+    {}
+};
 TICK_STATIC_TEST_CASE()
 {
     template<class T, TICK_REQUIRES(std::is_integral<T>::value)>
@@ -42,12 +49,61 @@ TICK_STATIC_TEST_CASE()
     STATIC_ASSERT_SAME(decltype(check_vardiac_requires(not_int())), std::false_type);
 };
 
+TICK_STATIC_TEST_CASE()
+{
+    template<class T, TICK_REQUIRES(tick_is_integral<T>())>
+    static std::true_type check_requires(T);
+
+    template<class T, TICK_REQUIRES(!tick_is_integral<T>())>
+    static std::false_type check_requires(T);
+
+    STATIC_ASSERT_SAME(decltype(check_requires(1)), std::true_type);
+    STATIC_ASSERT_SAME(decltype(check_requires(not_int())), std::false_type);
+};
+
+TICK_STATIC_TEST_CASE()
+{
+    template<class T, TICK_REQUIRES(tick_is_integral<T>())>
+    static std::true_type check_requires(T);
+
+    template<class T, TICK_REQUIRES(!tick_is_integral<T>())>
+    static std::true_type check_requires(T);
+
+    STATIC_ASSERT_SAME(decltype(check_requires(1)), std::true_type);
+    STATIC_ASSERT_SAME(decltype(check_requires(not_int())), std::true_type);
+};
+
+TICK_STATIC_TEST_CASE()
+{
+    template<class T, class... Ts, TICK_REQUIRES(tick_is_integral<T>())>
+    static std::true_type check_vardiac_requires(T, Ts...);
+
+    template<class T, class... Ts, TICK_REQUIRES(!tick_is_integral<T>())>
+    static std::false_type check_vardiac_requires(T, Ts...);
+
+    STATIC_ASSERT_SAME(decltype(check_vardiac_requires(1)), std::true_type);
+    STATIC_ASSERT_SAME(decltype(check_vardiac_requires(not_int())), std::false_type);
+};
 template<class T> 
 TICK_FUNCTION_REQUIRES(std::is_integral<T>::value)
-(std::true_type) check_function_requires(T);
+(std::true_type) check_function_requires_c(T);
 
 template<class T> 
 TICK_FUNCTION_REQUIRES(!std::is_integral<T>::value)
+(std::false_type) check_function_requires_c(T);
+
+TICK_STATIC_TEST_CASE()
+{
+    STATIC_ASSERT_SAME(decltype(check_function_requires_c(1)), std::true_type);
+    STATIC_ASSERT_SAME(decltype(check_function_requires_c(not_int())), std::false_type);
+};
+
+template<class T> 
+TICK_FUNCTION_REQUIRES(tick_is_integral<T>())
+(std::true_type) check_function_requires(T);
+
+template<class T> 
+TICK_FUNCTION_REQUIRES(!tick_is_integral<T>())
 (std::false_type) check_function_requires(T);
 
 TICK_STATIC_TEST_CASE()
@@ -69,6 +125,27 @@ TICK_STATIC_TEST_CASE()
 
     template<class T>
     struct check_class_requires<T, TICK_CLASS_REQUIRES(!std::is_integral<T>::value)>
+    {
+        static std::false_type call();
+    };
+
+    STATIC_ASSERT_SAME(decltype(check_class_requires<int>::call()), std::true_type);
+    STATIC_ASSERT_SAME(decltype(check_class_requires<not_int>::call()), std::false_type);
+};
+
+TICK_STATIC_TEST_CASE()
+{
+    template<class T, class=void>
+    struct check_class_requires;
+
+    template<class T>
+    struct check_class_requires<T, TICK_CLASS_REQUIRES(tick_is_integral<T>())>
+    {
+        static std::true_type call();
+    };
+
+    template<class T>
+    struct check_class_requires<T, TICK_CLASS_REQUIRES(!tick_is_integral<T>())>
     {
         static std::false_type call();
     };
@@ -127,16 +204,66 @@ TICK_STATIC_TEST_CASE()
     STATIC_ASSERT_SAME(decltype(check_member_requires<not_int>().foo()), int);
 };
 
-template<class T>
-struct is_integral
-: tick::integral_constant<bool, std::is_integral<T>::value>
-{};
+TICK_STATIC_TEST_CASE()
+{
+    template<class T>
+    struct check_member_requires
+    {
+        T x;
+
+        TICK_MEMBER_REQUIRES(tick_is_integral<T>())
+        std::true_type foo()
+        {
+            x += 1;
+            return std::true_type();
+        }
+
+        TICK_MEMBER_REQUIRES(!tick_is_integral<T>())
+        std::false_type foo()
+        {
+            return std::false_type();
+        }
+    };
+
+    STATIC_ASSERT_SAME(decltype(check_member_requires<int>().foo()), std::true_type);
+    STATIC_ASSERT_SAME(decltype(check_member_requires<not_int>().foo()), std::false_type);
+};
+
+TICK_STATIC_TEST_CASE()
+{
+    template<class T>
+    struct check_member_requires
+    {
+        T x;
+
+        TICK_MEMBER_REQUIRES(tick_is_integral<T>())
+        int foo()
+        {
+            x += 1;
+            return x;
+        }
+
+        TICK_MEMBER_REQUIRES(!tick_is_integral<T>())
+        int foo()
+        {
+            return 0;
+        }
+    };
+
+    STATIC_ASSERT_SAME(decltype(check_member_requires<int>().foo()), int);
+    STATIC_ASSERT_SAME(decltype(check_member_requires<not_int>().foo()), int);
+};
+
+// template<class T>
+// struct is_integral
+// : tick::integral_constant<bool, std::is_integral<T>::value>
+// {};
 
 template<class T>
-std::true_type check_param_requires(T, TICK_PARAM_REQUIRES(is_integral<T>()));
+std::true_type check_param_requires(T, TICK_PARAM_REQUIRES(tick_is_integral<T>()));
 
 template<class T>
-std::false_type check_param_requires(T, TICK_PARAM_REQUIRES(!is_integral<T>()));
+std::false_type check_param_requires(T, TICK_PARAM_REQUIRES(!tick_is_integral<T>()));
 
 TICK_STATIC_TEST_CASE()
 {
@@ -162,4 +289,3 @@ TICK_STATIC_TEST_CASE()
     STATIC_ASSERT_SAME(decltype(check_param_trait_requires(1)), std::true_type);
     STATIC_ASSERT_SAME(decltype(check_param_trait_requires(not_int())), std::false_type);
 };
-
