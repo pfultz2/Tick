59717f8d2a92477a8425887e792d6d0e53be1ed3
Paul <pfultz2@yahoo.com>
2017-01-12 00:49:30 -0600

    Add documentation for bidirectional iterator

--- tick/traits/is_bidirectional_iterator.h
+++ tick/traits/is_bidirectional_iterator.h
@@ -8,6 +8,53 @@
 #ifndef TICK_GUARD_IS_BIDIRECTIONAL_ITERATOR_H
 #define TICK_GUARD_IS_BIDIRECTIONAL_ITERATOR_H
 
+/// is_bidirectional_iterator
+/// =========================
+/// 
+/// Description
+/// -----------
+/// 
+/// A bidirectional iterator is a forward iterator that can be moved in both
+/// directions (i.e. incremented and decremented).
+/// 
+/// Requirements
+/// ------------
+/// 
+///  The type `It` satisfies `is_bidirectional_iterator` if
+///  
+///  * The type It satisfies [`is_forward_iterator`](is_forward_iterator)
+///  
+///  And, given
+///  * `a` and `b`, iterators of type `It`
+///  * `reference`, the type denoted by `std::iterator_traits<It>::reference`
+///  
+///  The following expressions must be valid and have their specified effects
+///  
+///  +--------------------+------------------------+------------------------------------+
+///  | Expression         | Return                 | Equivalent                         |
+///  |                    |                        | expression                         |
+///  +====================+========================+====================================+
+///  | ``--a``            | ``It&``                |                                    |
+///  +--------------------+------------------------+------------------------------------+
+///  | ``a00``            | convertible to ``It&`` | ``It temp = a; --a; return temp;`` |
+///  +--------------------+------------------------+------------------------------------+
+///  | ``*a--``           | ``reference``          |                                    |
+///  +--------------------+------------------------+------------------------------------+
+/// 
+/// Synopsis
+/// --------
+/// 
+///     TICK_TRAIT(is_bidirectional_iterator, is_forward_iterator<_>)
+///     {
+///         template<class I>
+///         auto require(I&& i) -> valid<
+///             returns<typename std::add_lvalue_reference<I>::type>(--i),
+///             returns<I>(i--),
+///             returns<typename iterator_traits<I>::reference>(*i--)
+///         >;
+///     };
+/// 
+
 #include <tick/builder.h>
 #include <tick/traits/is_forward_iterator.h>
 
